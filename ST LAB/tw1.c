72fe694b69147164480408a0f3adf8f8fc01bf09
Adarsh Kumbar <122893949+Adarshkumbar@users.noreply.github.com>
2023-12-26 00:02:20 +0530

    tw1.c

---
+++ ST LAB/tw1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter three sides of the triangle : ");
+    scanf("%d%d%d", &a, &b, &c);
+    
+    if((a<b+c)&&(b<a+c)&&(c<a+b))
+    {
+        if((a==b)&&(b==c))
+    {
+    printf("Equilateral triangle");
+    }
+    else if((a!=b)&&(a!=c)&&(b!=c))
+    {
+        printf("Scalene triangle");
+    }
+    else
+        printf("Isosceles triangle");
+    }
+    else
+    {
+        printf("triangle cannot be formed");
+    }
+
+return 0;
+}
